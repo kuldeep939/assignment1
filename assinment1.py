8c6c850f8fe231e34939f4c117e12bb797c706c4
kuldeep939 <kuldeepr939@gmail.com>
2025-11-29 20:19:31 +0530

    Implement basic mathematical operations in Python

---
+++ assinment1.py
@@ -0,0 +1,7 @@
+# Task 1: Perform Basic Mathematical Operations
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+print("Addition:",x+y)
+print("Subtraction:",x-y)
+print("Multiplication:",x*y)    
+print("Division:",x/y)
